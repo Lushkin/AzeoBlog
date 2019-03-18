63a1524a373b795344588eb7570691ad92a004a8
Roman Lushkin <rlushkin@neotech-solutions.fr>
2019-03-18 09:44:07 +0100

    Add InitWindow Script

---
+++ InitWindow.cs
@@ -0,0 +1,7 @@
+private void InitWindow()
+{
+    graphics.PreferredBackBufferWidth = 1280;  // set this value to the desired width of your window
+    graphics.PreferredBackBufferHeight = 720;   // set this value to the desired height of your window
+    graphics.IsFullScreen = false;
+    graphics.ApplyChanges();
+}
